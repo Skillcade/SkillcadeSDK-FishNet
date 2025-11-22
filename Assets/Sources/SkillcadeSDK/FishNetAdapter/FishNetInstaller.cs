eb90038ad9ecd4506599a2bf44f67792f20e667d
Aleksandr Litskevich <avlitskevich@gmail.com>
2025-11-22 16:40:26 +0500

    Added registering players controller

--- Assets/Sources/SkillcadeSDK/FishNetAdapter/FishNetInstaller.cs
+++ Assets/Sources/SkillcadeSDK/FishNetAdapter/FishNetInstaller.cs
@@ -2,6 +2,7 @@
 using FishNet.Transporting.UTP;
 using SkillcadeSDK.DI;
 using SkillcadeSDK.FishNetAdapter.PingService;
+using SkillcadeSDK.FishNetAdapter.Players;
 using UnityEngine;
 using VContainer;
 using VContainer.Unity;
@@ -13,12 +14,14 @@ namespace SkillcadeSDK.FishNetAdapter
         [SerializeField] private NetworkManager _networkManager;
         [SerializeField] private UnityTransport _unityTransport;
         [SerializeField] private FishNetConnectionController _connectionController;
+        [SerializeField] private FishNetPlayersController _playersController;
         
         public override void Install(IContainerBuilder builder)
         {
             builder.RegisterInstance(_networkManager);
             builder.RegisterInstance(_unityTransport);
             builder.RegisterInstance(_connectionController).AsSelf().AsImplementedInterfaces();
+            builder.RegisterInstance(_playersController).AsSelf().AsImplementedInterfaces();
 
             builder.RegisterEntryPoint<FishNetTransportAdapter>().AsSelf();
             builder.RegisterEntryPoint<FishNetPingService>();
