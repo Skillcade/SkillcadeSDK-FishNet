d65f7bc916b0df105a99bd013b6893e5636e8a1f
Aleksandr Litskevich <avlitskevich@gmail.com>
2026-01-27 13:15:58 +0500

    Removed default player spawner, cause it was used in specific game only

--- Assets/Sources/SkillcadeSDK/FishNetAdapter/SpawnPointProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace SkillcadeSDK.FishNetAdapter
-{
-    public class SpawnPointProvider : MonoBehaviour
-    {
-        public Vector3 Position => transform.position;
-    }
-}
