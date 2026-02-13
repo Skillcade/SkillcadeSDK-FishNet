56a816d661d3fc5d22335c3044064addff657eb1
Aleksandr Litskevich <avlitskevich@gmail.com>
2026-02-13 17:23:36 +0500

    Fixed FishNet compilation error

--- Assets/Sources/SkillcadeSDK/FishNetAdapter/ColliderRollback/RollbackTrigger.cs
+++ Assets/Sources/SkillcadeSDK/FishNetAdapter/ColliderRollback/RollbackTrigger.cs
@@ -4,12 +4,17 @@ namespace SkillcadeSDK.FishNetAdapter.ColliderRollback
 {
     /// <summary>
     /// Base class for all triggers that should be verified on server using ColliderRollback feature.
-    /// Implement HandleTriggerServer in your components.
+    /// Implement HandleTriggerServer_Internal in your components.
     /// playerNetworkObject is reference to NetworkObject of player, who called ColliderRollback verification.
     /// </summary>
     public abstract class RollbackTrigger : NetworkBehaviour
     {
         [Server]
-        public abstract void HandleTriggerServer(NetworkObject playerNetworkObject);
+        public void HandleTriggerServer(NetworkObject playerNetworkObject)
+        {
+            HandleTriggerServer_Internal(playerNetworkObject);
+        }
+
+        protected abstract void HandleTriggerServer_Internal(NetworkObject playerNetworkObject);
     }
 }
